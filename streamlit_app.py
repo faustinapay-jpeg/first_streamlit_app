e752c4decc54ae38aa1a065b48b0ada5497923e0
faustinapay-jpeg <84888274+faustinapay-jpeg@users.noreply.github.com>
2023-06-07 17:49:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLerror as e:
-streamlit.error()
+    streamlit.error()
 
     streamlit.header("The fruit load contains:")
 #Snowflake-related functions
